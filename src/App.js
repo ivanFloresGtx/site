85490550ae958c94cad615f12f09ebc0cfc15e15
Iván Gutiérrez <igutierrez.yunius@gmail.com>
2021-12-09 20:39:53 -0600

    arreglando conflictos

--- src/App.js
+++ src/App.js
@@ -7,23 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
-          Hello this is my website
+         Hello this is my website
         </p>
      </header>
->>>>>>> gh-pages
     </div>
   );
 }
